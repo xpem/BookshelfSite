ec7d123cf7e0a74c063a32323dcc2e8e5806bc3e
emanuel <emanuel@enotas.com.br>
2021-10-10 14:15:05 -0300

    iniciando as implementações das funções do bd

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/database';
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,6 +12,8 @@ const app = firebase.initializeApp({
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,  
     appId: process.env.REACT_APP_FIREBASE_API_ID,  
 });
-
+//for authentication functions
 export const auth = app.auth();
+//for database functions
+export const db = app.database();
 export default app;
