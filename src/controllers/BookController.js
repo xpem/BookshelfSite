387d737dc89af4cf48a1e0641322114079cd7daf
emanuel <emanuel@enotas.com.br>
2021-11-02 19:54:27 -0300

    wip book card for list component

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -11,4 +11,27 @@ export async function CreateBook(book) {
         console.log("erro ao adicionar" + error);
       });
   }
+
+  //
+  // export async function GetBookByTitleAndSubtitle(Title,Subtitle) {
+  //   var data = [];
+  //   try {
+  //    await db.ref("UsersBeta").orderByChild("Title").equalTo(Title).orderByChild("Subtitle").equalTo(Subtitle).once("value", (snapshot) => {
+  //       if (snapshot != null) {
+  //         snapshot.forEach((childSnapshot) => {
+  //           data.push({
+  //             id: childSnapshot.key,
+  //             ...childSnapshot.val(),
+  //           });
+  //         });     
+  //       }else{
+  //         console.log("tabela nula")
+  //       }  
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  //   return data;
+  // }
+
   
--- src/pages/Books/BooksList/index.js
+++ src/pages/Books/BooksList/index.js
@@ -5,8 +5,19 @@ export default function BookList() {
   return (
     <div>
       <div className="Main-Frame">
+      <div className="Card-Book">
+         <p className="p-title"><b>Teste de título</b></p>
+         <p className="p-subtitle">teste de subtítulo; Vol.: 13</p>
+         <p className="p-authoryear">Nome do Autor; Ano 2020</p>
+         <p className="p-pages">Páginas: 378</p>
+         <p className="p-rate">Avaliação pessoal: 3 de 5</p>
+        </div>
         <div className="Card-Book">
-         <h1>Teste</h1>
+         <p className="p-title"><b>Teste de título</b></p>
+         <p className="p-subtitle">teste de subtítulo; Vol.: 13</p>
+         <p className="p-authoryear">Nome do Autor; Ano 2020</p>
+         <p className="p-pages">Páginas: 378</p>
+         <p className="p-rate">Avaliação pessoal: 3 de 5</p>
         </div>
       </div>
     </div>
